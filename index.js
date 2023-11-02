62e061391e57d6a04971be8255093cc2a1e7362b
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-02 16:37:04 +0530

    Rendered App

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import ReactDOM from "react-dom/client";
+import App from "./src/App";
 
-ReactDOM.createRoot(document.querySelector("#root")).render(
-  <h1>Password Generator</h1>
-);
+ReactDOM.createRoot(document.querySelector("#root")).render(<App />);
