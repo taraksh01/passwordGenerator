0420f1da4ecaf9da88e9f373ef8ab6beeb895d08
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-11-02 16:36:48 +0530

    Added App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className="bg-gray-600 text-gray-200 h-screen w-screen">
+      <div className="bg-slate-900 max-w-xl rounded-md mx-auto p-4 ">
+        <h1 className="text-xl">Password Generator</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
